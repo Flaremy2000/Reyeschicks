b2d0b09ccead06da5d20f0a8d5455323409f33b2
Flaremy <jeremyponce2016@gmail.com>
2021-08-17 18:35:16 -0500

    Ventana Flotante para iniciar sesion

--- public/js/control.js
+++ public/js/control.js
@@ -27,6 +27,6 @@ function Contacto(){
 }
 
 function cargado() {
-    alert("Se mostro esto al cargar la pagina");
+    document.body.innerHTML += "<div class=\"modal fade\" id=\"EditingUser\" data-bs-backdrop=\"static\" data-bs-keyboard=\"false\" tabindex=\"-1\" aria-labelledby=\"staticBackdropLabel\" aria-hidden=\"true\"><div class=\"modal-dialog\"><div class=\"modal-content\"><div class=\"modal-header\"><h5 class=\"modal-title\" id=\"staticBackdropLabel\">Iniciar Sesion</h5><button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button></div><div class=\"modal-body\"><div class=\"formulario de registro\"><form><label>Cedula: <input type=\"number\" size=\"10\" required></label><label>Password: <input type=\"password\" size=\"20\" required disabled></label><br></form></div></div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Cerrar</button><button id=\"login\" type=\"button\" class=\"btn btn-primary\">Iniciar</button></div></div></div></div>"
 }
 document.window.onload = cargado();
